c01955095a4104a03195969967b3c265c36d4bee
Dave Yarwood <dave.yarwood@gmail.com>
2022-01-03 23:26:48 -0500

    Misc. sequence generation tweaks
    * Include the starting position in the sequence
    * Throw an error with a good message if there are unexpectedly no options from a
      position (other than Submitted)

--- src/bjj_graph/generator.clj
+++ src/bjj_graph/generator.clj
@@ -11,7 +11,7 @@
                                 label)]]
     [label' dest]))
 
-(defn random-sequence
+(defn- random-sequence*
   "Given a starting position, generates a random sequence of techniques that
    can be used logically in sequence.
 
@@ -21,9 +21,29 @@
   (if (= "Submitted" position)
     '()
     (lazy-seq
-      (let [[technique next-position] (rand-nth (options position))]
-        (cons technique (random-sequence next-position))))))
+      (let [next-position-options
+            (options position)
+
+            _
+            (when-not (pos? (count next-position-options))
+              (throw (ex-info "No options available from position"
+                              {:position position})))
+
+            [technique next-position]
+            (rand-nth (options position))]
+        (cons technique (random-sequence* next-position))))))
+
+(defn random-sequence
+  "Given a starting position, generates a random sequence of techniques that
+   can be used logically in sequence.
+
+   The sequence is of arbitrary length, terminating whenever \"Submitted\" is
+   reached."
+  [position]
+  (cons position (random-sequence* position)))
 
 (comment
   (random-sequence "Standing Apart")
-  (random-sequence "Mount"))
+  (random-sequence "Mount")
+  (random-sequence (rand-nth (uber/nodes bjj/GRAPH)))
+  *e)
