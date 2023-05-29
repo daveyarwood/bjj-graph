cbd570d8cc08984193d43749a15cb8657c2a0fe0
Dave Yarwood <dave.yarwood@gmail.com>
2023-05-29 01:55:07 -0400

    Scratch code that regenerates subgraphs forever

--- dev/user.clj
+++ dev/user.clj
@@ -3,14 +3,35 @@
             [bjj-graph.generator  :as gen]
             [clojure.java.process :as proc]
             [ubergraph.core       :as uber])
-  (:import [java.time LocalDate]))
+  (:import [java.awt.event WindowListener]
+           [java.time LocalDate]
+           [javax.swing JFrame]))
+
+(defn- make-jframes-forever
+  "Repeatedly calls `jframe-fn`, a function that creates a JFrame. Each new
+   JFrame is created as the previous one is closed.
+
+   (The only way to make this stop is to Ctrl-C your REPL.)"
+  [jframe-fn]
+  (let [^JFrame jframe (jframe-fn)]
+    (.addWindowListener
+      jframe
+      (reify WindowListener
+        (windowActivated [_ _e])
+        (windowClosing [_ _e] (make-jframes-forever jframe-fn))
+        (windowClosed [_ _e])
+        (windowDeactivated [_ _e])
+        (windowDeiconified [_ _e])
+        (windowIconified [_ _e])
+        (windowOpened [_ _e])))))
 
 (comment
   (uber/pprint bjj/GRAPH)
 
-  (uber/viz-graph
-    (gen/random-subgraph "Guard" 3)
-    {:layout :dot})
+  (make-jframes-forever
+    #(uber/viz-graph
+       (gen/random-subgraph "Guard" 3)
+       {:layout :dot}))
 
   (bjj/viz-graph {})
 
